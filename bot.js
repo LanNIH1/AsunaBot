489eb3868871032d44a8741f469d2667137c5565
LanNIH1 <dr45698@gmail.com>
2022-11-01 02:22:16 +0800

    更改內容簡介

--- bot.js
+++ bot.js
@@ -11,6 +11,9 @@ const client = new Discord.Client();
 // 聊天字典(DB)
 const messageData = require('./messageData.json');
 
+//嵌入內容
+const { EmbedBuilder } = require('discord.js');
+
 // 登入
 client.login(process.env.TOKEN);
 
@@ -29,4 +32,29 @@ client.on('message', msg => {
     }
   }
 });
+
+const exampleEmbed = new EmbedBuilder()
+	.setColor(9370db)
+	.setTitle('SGO CBT2 樓層情報')
+	.setAuthor({ name: 'Argo', iconURL: 'https://imgur.com/LKMOgsP.jpg', url: 'https://swordgale.online/' })
+	.setDescription('SGO CBT2 樓層怪物情報（非完整）')
+	.setThumbnail('https://imgur.com/bDEucFO.jpg')
+	.addFields(
+		{ name: '封測樓層說明', value: '封測共開了六層，無奈實力不足只踏足了四層，且有許多未收集完整的情報' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: '第一層', value: '大草原', inline: true },
+		{ name: '第二層', value: '猛牛園', inline: true },
+    { name: '第三層', value: '兒童樂園', inline: true },
+    { name: '\u200B', value: '\u200B' },
+    { name: '第四層', value: '菇菇園', inline: true },
+		{ name: '第五層', value: '圓明園', inline: true },
+    { name: '第六層', value: '封測限定對戰塔', inline: true },
+	)
+	.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setTimestamp()
+	.setFooter({ text: 'writen by Argo', iconURL: 'https://imgur.com/bDEucFO.jpg' });
+
+channel.send({ embeds: ['SGO 樓層'] });
+
 
