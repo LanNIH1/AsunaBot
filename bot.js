95674eb798429221448913bf56cc3ebd44278ef8
LanNIH1 <dr45698@gmail.com>
2022-11-01 18:44:50 +0800

    更改內容簡介

--- bot.js
+++ bot.js
@@ -68,5 +68,36 @@ client.on('message', message => {
     .setImage('https://imgur.com/pdA0tf1.jpg');
     message.channel.send(embed);
   }});
-  
 
+    
+  client.on('message', message => {
+    //メッセージを入る
+    if (message.content === 'sgo help') {
+      const embed = new Discord.MessageEmbed()
+      //タイトルを設定する
+      .setTitle('SGO CBT2 資訊一覽')
+      //エっジカーラーを設定する
+      .setColor(0x660099)
+      //内容を設定する
+      .setDescription(
+        '各種資訊導引' + '\n' + '\n' +  '\n' + '\n'+
+        '**樓層指南**' +'\n' +
+        'sgo floor' + '\n' + '\n' +
+        '**Makushi的攻略情報**' +'\n' +
+        '新手鍛造入門' + '　　' +'前期短刀維生' + '　　' + '開局走向' +'\n' + '\n' + '\n'+
+        '**資料仍有缺漏，僅供參考**' + '\n' + '\n' +'\n' ) 
+      //作者のアイコン、タイトル、url
+      .setAuthor('Argo', 'https://imgur.com/bDEucFO.jpg', 'https://swordgale.online/')
+      //右上のサムネイルがどこかわからない
+      .setThumbnail('https://imgur.com/pdA0tf1.jpg')
+      //下の文字、画像
+      .setFooter('Writen by Argo','https://imgur.com/bDEucFO.jpg')
+      //時間表示
+      .setTimestamp(Date.now())
+      //タイトルURL
+      //.setURL('https://www.youtube.com/');
+      //設定圖片
+      //.setImage('https://imgur.com/pdA0tf1.jpg');
+      message.channel.send(embed);
+    }});
+  
