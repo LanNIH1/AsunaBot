2dd28a13f78f45ca4dada1b6514b5ede4789918a
LanNIH1 <dr45698@gmail.com>
2022-11-03 19:38:55 +0800

    更改內容簡介

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 // 健康狀態
 require("./jsHome/healthCheck.js").start();
 // 定時作業工具
-const cornTask = require('./jsHome/cronTask.js');
+const cornTask = require("./jsHome/cronTask.js");
 // discord library
 const Discord = require("discord.js");
 const client = new Discord.Client();
@@ -27,7 +27,7 @@ client.on("ready", () => {
 });
 
 // messageData 訊息查找&回答
-client.on('message', msg => {
+client.on("message", msg => {
   for (let i = 0; i < messageData?.length; i++) {
     if (msg.content === messageData[i]?.content) {
       msg.reply(messageData[i]?.reply);
@@ -36,68 +36,70 @@ client.on('message', msg => {
   }
 });
 
-client.on('message', message => {
+client.on("message", message => {
   //メッセージを入る
-  if (message.content === 'sgo floor') {
+  if (message.content === "sgo floor") {
     const embed = new Discord.MessageEmbed()
     //タイトルを設定する
-    .setTitle('SGO CBT2 樓層怪物一覽')
+    .setTitle("SGO CBT2 樓層怪物一覽")
     //エっジカーラーを設定する
     .setColor(0x660099)
     //内容を設定する
     .setDescription(
-      '**資料仍有缺漏，僅供參考**' + '\n' + '\n' +'\n' +
-      '－－－－－－－－－' + '\n' + '\n' +  '\n' +
-      '第一層：大草原' + '　　' +'\n' + '\n' +
-      '第二層：猛牛園' + '　　' + '\n' + '\n' +
-      '第三層：兒童樂園' + '　　' +'\n' + '\n' +
-      '第四層：蘑菇園' + '　　' + '\n' + '\n' +
-      '第五層：圓明園' + '　　' +'\n' + '\n' +
-      '第六層：封測限定對戰塔' + '　　' + '\n' + '\n') 
+      "**資料仍有缺漏，僅供參考**" + "\n" + "\n" +"\n" +
+      "－－－－－－－－－" + "\n" + "\n" +  "\n" +
+      "第一層：大草原" + "　　" +"\n" + "\n" +
+      "第二層：猛牛園" + "　　" + "\n" + "\n" +
+      "第三層：兒童樂園" + "　　" +"\n" + "\n" +
+      "第四層：蘑菇園" + "　　" + "\n" + "\n" +
+      "第五層：圓明園" + "　　" +"\n" + "\n" +
+      "第六層：封測限定對戰塔" + "　　" + "\n" + "\n") 
     //作者のアイコン、タイトル、url
-    .setAuthor('Argo', 'https://imgur.com/bDEucFO.jpg', 'https://swordgale.online/')
+    .setAuthor("Argo", "https://imgur.com/bDEucFO.jpg", "https://swordgale.online/")
     //右上のサムネイルがどこかわからない
     //.setThumbnail(https://imgur.com/LKMOgsP.jpg)
     //下の文字、画像
-    .setFooter('Writen by Argo','https://imgur.com/bDEucFO.jpg')
+    .setFooter("Writen by Argo","https://imgur.com/bDEucFO.jpg")
     //時間表示
     .setTimestamp(Date.now())
     //タイトルURL
-    //.setURL('https://www.youtube.com/');
+    //.setURL("https://www.youtube.com/");
     //設定圖片
-    .setImage('https://imgur.com/pdA0tf1.jpg');
+    .setImage("https://imgur.com/pdA0tf1.jpg");
     message.channel.send(embed);
   }});
 
     
-  client.on('message', message => {
+  client.on("message", message => {
     //メッセージを入る
-    if (message.content === 'sgo help') {
+    if (message.content === "sgo help") {
       const embed = new Discord.MessageEmbed()
       //タイトルを設定する
-      .setTitle('SGO CBT2 資訊一覽')
+      .setTitle("SGO CBT2 資訊一覽")
       //エっジカーラーを設定する
       .setColor(0x660099)
       //内容を設定する
       .setDescription(
-        '**各種資訊導引**' + '\n' + '\n' +  '\n' + '\n'+
-        '**樓層指南**' +'\n' +
-        'sgo floor' + '\n' + '\n' +
-        '**Makusi的攻略情報**' +'\n' +
-        '新手鍛造入門' + '　　' +'前期短刀維生' + '　　' + '開局走向' +'\n' + '\n' + '\n'+
-        '**資料仍有缺漏，僅供參考**' + '\n' + '\n' +'\n' ) 
+        "**各種資訊導引**" + "\n" + "\n" +  "\n" + "\n"+
+        "**樓層指南**" +"\n" +
+        "sgo floor" + "\n" + "\n" +
+        "**Makusi的攻略情報**" +"\n" +
+        "新手鍛造入門" + "　　" +"前期短刀維生" + "　　" + "開局走向" +"\n" + "\n" + "\n"+
+        "**資料仍有缺漏，僅供參考**" + "\n" + "\n" +"\n" ) 
       //作者のアイコン、タイトル、url
-      .setAuthor('Argo', 'https://imgur.com/bDEucFO.jpg', 'https://swordgale.online/')
+      .setAuthor("Argo", "https://imgur.com/bDEucFO.jpg", "https://swordgale.online/")
       //右上のサムネイルがどこかわからない
-      .setThumbnail('https://imgur.com/pdA0tf1.jpg')
+      .setThumbnail("https://imgur.com/pdA0tf1.jpg")
       //下の文字、画像
-      .setFooter('Writen by Argo','https://imgur.com/bDEucFO.jpg')
+      .setFooter("Writen by Argo","https://imgur.com/bDEucFO.jpg")
       //時間表示
       .setTimestamp(Date.now())
       //タイトルURL
-      //.setURL('https://www.youtube.com/');
-      //設定圖片
-      //.setImage('https://imgur.com/pdA0tf1.jpg');
+      //.setURL("https://www.youtube.com/");
+      //圖片を設定する
+      //.setImage("https://imgur.com/pdA0tf1.jpg");
       message.channel.send(embed);
     }});
+
+
   
